50c406896979ceff402c42f292999639a715877f
jeph <nicjef@gmail.com>
2017-02-04 14:27:05 +0530

    add new annotation image

---
+++ GetCurrentLocation/CustomAnnotationView.swift
@@ -0,0 +1,21 @@
+//
+//  CustomAnnotationView.swift
+//  GetCurrentLocation
+//
+//  Created by Jeph B on 04/02/2017.
+//  Copyright © 2017 Jeph B. All rights reserved.
+//
+
+import UIKit
+
+class CustomAnnotationView: MKAnnotationView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ GetCurrentLocation/MyLocation.swift
@@ -0,0 +1,21 @@
+//
+//  MyLocation.swift
+//  GetCurrentLocation
+//
+//  Created by Jeph B on 04/02/2017.
+//  Copyright © 2017 Jeph B. All rights reserved.
+//
+
+import UIKit
+
+class MyLocation: MKAnnotationView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
